79594576a8d3fe242e132c71f78c0de8017d2a6e
santiago b <santiagobadaya@gmail.com>
2023-05-17 22:51:35 +0200

    first draft

---
+++ src/Calculator.java
@@ -0,0 +1,24 @@
+/**
+ * The main class of a simple calculator. Create one of these, and you'll
+ * get the calculator on screen.
+ *
+ * @author David J. Barnes and Michael Kolling
+ * @version 2008.03.30
+ */
+public class Calculator
+{
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
+
+    /**
+     * Create a new calculator and show it.
+     */
+    private Calculator()
+    {
+        CalcEngine engine = new CalcEngine();
+        new UserInterface(engine);
+    }
+
+}
